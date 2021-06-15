91bd0b9c14eac810334360e36ecf6d682d61a628
msncc <77657087+msnco@users.noreply.github.com>
2021-06-15 21:16:26 +0800

    1

--- js/all.js
+++ js/all.js
@@ -250,7 +250,7 @@ function setpage() {   // 初始化or重繪 頁面列表
 
 /*create page list*/
 var valTwo = showNums;
-var indexVal = 0;
+var indexVal = 0,ranNum = 0;
 var curView = 1;
 var gTstr = "";
 function gotopage(nextNums){
@@ -268,9 +268,14 @@ function gotopage(nextNums){
 	if(arr.length < valTwo){
 		valTwo = arr.length;
 	}
+	ranNum = Math.floor(Math.random() * (valTwo - 1)) + 1; //max - min + min
 	for(var i=indexVal; i<valTwo; i++){
+		if(i != ranNum){
 		gTstr = arr[i].substring(0,1);
 		creatImg += "<li><a href='file/"+fName+arr[i]+".html'> <div class='imgSize'><img class='lazy' src='imgs/load.gif' data-echo='picture/view/"+fName+arr[i]+".jpg'/></div></a><div class='newsAllTitle'><p>"+arr0[i]+"</p><span class='tView'>"+dtp[gTstr-1][arr[i]][0]["1"]+"</span></div></li>";
+		}else{
+			creatImg += '<ins class="adsbygoogle" style="display:block;margin-top:10px;" data-ad-format="fluid" data-ad-layout-key="-d0-8q+1h+e7+kh" data-ad-client="ca-pub-1937515231401291" data-ad-slot="7813549177"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
+		}
 	}
 	creatLists = creatLists + creatImg;
 	By("newsAll").innerHTML = creatLists;
